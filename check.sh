fa45645296c390b6a57ef358ab10e4bcc3c96d53
Antonio Tsai <jimmyjack7777@gmail.com>
2017-06-19 00:52:18 +0800

    Generate a txt of repeated files

--- check.sh
+++ check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 source config.sh
+
 # Generate md5 of assignment
-find $directory -name $filetype -exec md5 {} + > md5sums.txt
+find $directory -name $filetype -exec md5 {} + > $md5sums
+
+# Check repeated
+node checkCheat.js $md5sums > $repeated_list
