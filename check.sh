d4cbaafd9d30ca0c08cf069ce8a95be775362af2
Antonio Tsai <jimmyjack7777@gmail.com>
2017-06-19 00:57:14 +0800

    Update variable's name

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 source config.sh
 
 # Generate md5 of assignment
-find $directory -name $filetype -exec md5 {} + > $md5sums
+find $directory -name $filetype -exec md5 {} + > $md5_list
 
 # Check repeated
-node checkCheat.js $md5sums > $repeated_list
+node checkCheat.js $md5_list > $repeated_list
